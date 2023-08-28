5e76b45b55a624ffb5d8a76768a7e661b870952a
mahirjain8959 <133571797+mahirjain8959@users.noreply.github.com>
2023-08-28 15:37:41 +0530

    Add files via upload

---
+++ assweak1.cpp
@@ -0,0 +1,35 @@
+// question number 1
+#include <iostream>
+using namespace std;
+int main(){
+     cout << "physics" << endl;
+     cout << "Wallah" <<endl;
+
+// question number 2
+    int x = 5 , y = 2;
+    int result = x * y ;
+    cout << result << endl;
+
+
+// question number 3
+// the space of int are 4 bit and floot are also 4 bit and bool are 1 bit
+
+// question number 4
+int a = 4;
+int b = 5;
+a++ ;
+b--;
+cout << "a = "<< ++a <<" b = "<< b-- << endl ;
+ 
+ // question number 5
+float pi = 3.14 ;
+float radius = 10;
+float circumferenace ;
+circumferenace = 2 * pi * radius ;
+cout << "the circumference of circle is :";
+cout << circumferenace;
+
+// question number 6
+// no one is variable name
+}
+
